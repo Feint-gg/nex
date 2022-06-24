8920601c69e89eb9a1208ab1eaa5db2d5dd2613e
Steele Scott <60533996+steele123@users.noreply.github.com>
2022-06-24 15:39:45 -0400

    Add github icon to navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,26 +1,31 @@
+import Link from "next/link";
+import GithubIcon from "./GithubIcon";
+
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-    return (
-       <div>
-            <header className="bg-primary-100 relative z-30 border-b flex items-center p-5">
-                <a className="text-lg font-semibold">
-                    Feint
-                </a>
-                <div className="mx-auto">
-                    <input placeholder="Search for a summoner..." className="p-0.5 pl-4 w-96" />
-                </div>
-            </header>
-            <main>
-                {children}
-            </main>
-            <footer className="bg-primary-100 p-5 border-t">
-                <div className="text-center">
-                    Feint.gg © {new Date().getFullYear()}
-                </div>
-            </footer>
-       </div>
-    )
+  return (
+    <div>
+      <header className="bg-primary-100 relative z-30 border-b flex items-center p-5">
+        <a className="text-lg font-semibold">Feint</a>
+        <div className="mx-auto">
+          <input
+            placeholder="Search for a summoner..."
+            className="p-0.5 pl-4 w-96"
+          />
+        </div>
+        <div className="ml-auto">
+          <a href="https://github.com/feint-gg/nex">
+            <GithubIcon />
+          </a>
+        </div>
+      </header>
+      <main>{children}</main>
+      <footer className="bg-primary-100 p-5 border-t">
+        <div className="text-center">Feint.gg © {new Date().getFullYear()}</div>
+      </footer>
+    </div>
+  );
 }
